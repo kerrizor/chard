3e283502926db3a8c98e9ec1a81317f8ca72c845
Kerri Miller <kerrizor@github.com>
2016-04-27 11:48:12 -0700

    Basic exercise

---
+++ exercise_3/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int age = 41;
+    int height = 180;
+
+    printf("I am %d years old\n", age );
+    printf("I am %d cm tall\n", height);
+
+    return 0;
+}
